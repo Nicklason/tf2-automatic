c397f3c673d61d0b13c4e73c8d075c76772a98f8
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2018-06-23 19:14:35 +0200

    Increased delay to avoid missing images

--- app/screenshot.js
+++ app/screenshot.js
@@ -7,7 +7,7 @@ const FOLDER_NAME = 'images';
 
 let OPTIONS = {
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
-    renderDelay: 100,
+    renderDelay: 2000,
     cookies: []
 };
 
