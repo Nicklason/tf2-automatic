3d8b5aa17116de98a576ce6af39a1434f596f97b
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2019-02-03 15:14:05 +0100

    Accidentally reverted gift offer changes
    lol

--- app/trade.js
+++ app/trade.js
@@ -203,8 +203,8 @@ function handleOffer (offer) {
 
     if (offer.isOneSided()) {
         if (offer.isGift() && config.get('acceptGifts') == true) {
-            offer.log('trade', 'is a gift offer asking for nothing in return, accepting');
-            Automatic.alert('trade', 'Gift offer asking for nothing in return, accepting');
+            offer.log('trade', 'by ' + offer.partner() + ' is a gift offer asking for nothing in return, accepting');
+            Automatic.alert('trade', 'by ' + offer.partner() + ' is a gift offer asking for nothing in return, accepting');
 
             offer.accept().then(function (status) {
                 offer.log('trade', 'successfully accepted' + (status == 'pending' ? '; confirmation required' : ''));
@@ -212,8 +212,8 @@ function handleOffer (offer) {
                 offer.log('warn', `could not be accepted: ${err}`);
             });
         } else {
-            offer.log('trade', 'is a gift offer, declining');
-            Automatic.alert('Gift offer, declining');
+            offer.log('trade', 'by ' + offer.partner() + ' is a gift offer, declining');
+            Automatic.alert('Gift offer by ' + offer.partner() + ', declining');
 
             offer.decline().then(function () {
                 offer.log('debug', 'declined');
