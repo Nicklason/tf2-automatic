cb673684a7f75ce6bbe07409e07bb18f2449d40f
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-10-26 22:41:47 +0200

    Add crate series to item

--- app/utils/item/getItem.js
+++ app/utils/item/getItem.js
@@ -1,5 +1,6 @@
 const SKU = require('tf2-sku');
 const url = require('url');
+const isObject = require('isobject');
 
 const schemaManager = require('lib/tf2-schema');
 
@@ -17,8 +18,48 @@ module.exports = function () {
     };
 
     // Adds missing properties
-    return SKU.fromString(SKU.fromObject(item));
+    return fixItem(SKU.fromString(SKU.fromObject(item)));
 };
+
+function fixItem (item) {
+    const schemaItem = schemaManager.schema.getItemByDefindex(item.defindex);
+
+    if (schemaItem.item_class === 'supply_crate') {
+        // The item is a case / crate, search for crate series
+
+        let series = null;
+
+        if (schemaItem.attributes !== undefined) {
+            for (let i = 0; i < schemaItem.attributes.length; i++) {
+                const attribute = schemaItem.attributes[i];
+
+                if (attribute.name === 'set supply crate series') {
+                    series = attribute.value;
+                }
+            }
+        }
+
+        if (series === null) {
+            // Find item in items_game
+
+            const itemsGameItem = schemaManager.schema.raw.items_game.items[item.defindex];
+
+            if (itemsGameItem.static_attrs !== undefined && itemsGameItem.static_attrs['set supply crate series'] !== undefined) {
+                if (isObject(itemsGameItem.static_attrs['set supply crate series'])) {
+                    series = itemsGameItem.static_attrs['set supply crate series'].value;
+                } else {
+                    series = itemsGameItem.static_attrs['set supply crate series'];
+                }
+            }
+        }
+
+        if (series !== null) {
+            item.crateseries = parseInt(series);
+        }
+    }
+
+    return item;
+}
 
 /**
  * Gets the defindex of an item
