91bb83c2b14b5ead2e6f4ead717be22aa45f21d3
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-10-27 21:34:02 +0100

    Added inventory example

---
+++ app/handler/examples/inventories.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-console */
+
+const TradeOfferManager = require('steam-tradeoffer-manager');
+
+const manager = require('lib/manager');
+
+exports.onRun = function (done) {
+    console.log('The bot is starting...');
+
+    done();
+};
+
+exports.onShutdown = function (done) {
+    console.log('The bot is stopping...');
+
+    done();
+};
+
+exports.onReady = function () {
+    console.log('Everything is ready!');
+
+    manager.loadInventory(440, 2, true, function (err, inventory) {
+        if (err) {
+            console.log('Error occurred while loading inventory: ' + err.message);
+            return;
+        }
+
+        inventory.forEach(function (item) {
+            console.log(`sku: ${item.getSKU()}, name: ${item.getName()}`);
+        });
+    });
+};
+
+exports.onLoginThrottle = function (wait) {
+    console.log('Waiting ' + wait + ' ms before trying to sign in...');
+};
+
+exports.onLoginSuccessful = function () {
+    console.log('Logged into Steam!');
+};
+
+exports.onLoginFailure = function (err) {
+    console.log('An error occurred while trying to sign into Steam: ' + err.message);
+
+    exports.shutdown();
+};
+
+exports.onLoginKey = function (loginKey) {};
+
+exports.onTradeOfferUpdated = function (offer, oldState) {
+    if (offer.state === TradeOfferManager.ETradeOfferState.Active && !offer.isOurOffer) {
+        onNewTradeOffer.call(this, offer);
+    } else if (oldState !== null) {
+        console.log('Offer #' + offer.id + ' state changed: ' + TradeOfferManager.ETradeOfferState[oldState] + ' -> ' + TradeOfferManager.ETradeOfferState[offer.state]);
+    }
+};
+
+function onNewTradeOffer (offer) {
+    console.log('Received an offer from ' + offer.partner.getSteamID64());
+}
+
+exports.onPollData = function (pollData) {};
+
+exports.onSchema = function (schema) {};
+
+exports.onLoginAttempts = function (attempts) {};
