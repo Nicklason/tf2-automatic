3a7ce50c38c22d19b52c25ecc6b787a68c4a074f
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-12-11 19:14:59 +0100

    Revert "Merge branch 'v2' into bot-framework"
    This reverts commit 75bb67b8a48837c9941b627dcb15d59d3eba5363, reversing
    changes made to 8d468638db4f28ea395a4461d5dce5ce88bb193d.

--- app/resources/paths.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    loginKey: path.join(__dirname, '../../files/loginkey.txt'),
-    pollData: path.join(__dirname, '../../files/polldata.json'),
-    loginAttempts: path.join(__dirname, '../../files/loginattempts.json')
-};
