96500dd3073758d43815b107ed1cbb72d7b9aef8
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-12-19 00:52:43 +0100

    Fixed creating listings

--- app/handler/listings.js
+++ app/handler/listings.js
@@ -1,3 +1,5 @@
+
+const log = require('lib/logger');
 const prices = require('app/prices');
 const inventory = require('app/inventory');
 const listingManager = require('lib/bptf-listings');
@@ -7,35 +9,57 @@ const templates = {
     sell: process.env.BPTF_DETAILS_SELL || 'I am selling my %name% for %price%, I have %current_stock%.'
 };
 
-exports.checkBySKU = function (sku, data) {
-    const match = data === undefined ? prices.get(sku) : data;
+exports.checkBySKU = function (sku) {
+    const match = prices.get(sku, true);
 
     let hasBuyListing = false;
     let hasSellListing = false;
 
+    const amountCanBuy = inventory.amountCanTrade(sku, true);
+    const amountCanSell = inventory.amountCanTrade(sku, false);
+
     listingManager.findListings(sku).forEach((listing) => {
-        if (match === null || match.intent !== 2 && match.intent !== listing.intent || match.enabled === false) {
+        if (listing.intent === 1 && hasSellListing) {
+            // Already have a sell order
+            log.debug('Already have a sell order, removing duplicate');
+            listing.remove();
+        }
+
+        if (listing.intent === 0) {
+            hasBuyListing = true;
+        } else if (listing.intent === 1) {
+            hasSellListing = true;
+        }
+
+        if (match === null || (match.intent !== 2 && match.intent !== listing.intent) || match.enabled === false) {
+            // We are not trading the item, remove the listing
+            log.debug('Removing listing because we are not trading the item', listing);
+            listing.remove();
+        } else if ((listing.intent === 0 && amountCanBuy <= 0) || (listing.intent === 1 && amountCanSell <= 0)) {
+            // We are not buying / selling more, remove the listing
+            log.debug('Removing listing because we are not buying / selling more');
             listing.remove();
         } else {
-            if (listing.intent === 0) {
-                hasBuyListing = true;
-            } else if (listing.intent === 1) {
-                hasSellListing = true;
+            const newDetails = getDetails(listing.intent, match);
+            if (listing.details !== newDetails) {
+                log.debug('Listing details are outdated, updating it');
+                // Listing details don't match, update listing with new details and price
+                const currencies = match[listing.intent === 0 ? 'buy' : 'sell'];
+                listing.update({
+                    details: getDetails(listing.intent, match),
+                    currencies: currencies
+                });
             }
-
-            const intent = match[listing.intent === 0 ? 'buy' : 'sell'];
-            const currencies = match[intent];
-            listing.update({
-                details: getDetails(listing.intent, match),
-                currencies: currencies
-            });
         }
     });
 
     if (match !== null && match.enabled === true) {
         const items = inventory.findBySKU(sku);
 
-        if (!hasBuyListing && (match.intent === 0 || match.intent === 2) && !inventory.isOverstocked(sku)) {
+        // TODO: Check if we are already making a listing for same type of item + intent
+
+        if (!hasBuyListing && (match.intent === 0 || match.intent === 2) && amountCanBuy > 0) {
+            log.debug('We can buy items, create listing');
             listingManager.createListing({
                 sku: sku,
                 intent: 0,
@@ -44,9 +68,10 @@ exports.checkBySKU = function (sku, data) {
             });
         }
 
-        if (!hasSellListing && (match.intent === 1 || match.intent === 2) && items.length - items.min > 0) {
+        if (!hasSellListing && (match.intent === 1 || match.intent === 2) && amountCanSell > 0) {
+            log.debug('We can sell items, create listing');
             listingManager.createListing({
-                sku: items[0],
+                id: items[items.length - 1],
                 intent: 1,
                 details: getDetails(1, match),
                 currencies: match.sell
@@ -68,8 +93,8 @@ function getDetails (intent, pricelistEntry) {
     const details = templates[intent === 0 ? 'buy' : 'sell']
         .replace(/%price%/g, pricelistEntry[key].toString())
         .replace(/%name%/g, pricelistEntry.name)
-        .replace(/%current_stock%/g, pricelistEntry.max - pricelistEntry.min)
-        .replace(/%max_stock%/g, inventory.getAmount(pricelistEntry.sku));
+        .replace(/%max_stock%/g, pricelistEntry.max - pricelistEntry.min)
+        .replace(/%current_stock%/g, inventory.getAmount(pricelistEntry.sku));
 
     return details;
 }
