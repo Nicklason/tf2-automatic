cecc49ad1ce062803b1fa1cfd9e2138b6d467699
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2019-12-30 19:17:55 +0100

    Validate admin steamids

--- app/admins.js
+++ app/admins.js
@@ -1,6 +1,14 @@
+const SteamID = require('steamid');
+
 const client = require('lib/client');
 
 const admins = process.env.ADMINS === undefined ? [] : JSON.parse(process.env.ADMINS);
+
+admins.forEach(function (steamid64) {
+    if (!new SteamID(steamid64).isValid()) {
+        throw new Error('Invalid admin SteamID64 "' + steamid64 + '"');
+    }
+});
 
 exports.message = function (message) {
     admins.forEach(function (steamID) {
