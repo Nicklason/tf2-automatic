a379b28ef6551e08090f8011f17bd43d8c629241
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-01-05 16:05:17 +0100

    Added restart command with PM2

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ try {
 const dotenv = require('dotenv');
 dotenv.config();
 
+const log = require('lib/logger');
+
+if (process.env.pm_id === undefined) {
+    log.warn('You are not running the bot with PM2! If the bot crashes it won\'t start again, see the documentation: https://github.com/Nicklason/tf2-automatic/wiki/PM2');
+}
+
 const handlerManager = require('app/handler-manager');
 handlerManager.setup();
 
@@ -28,8 +34,6 @@ const TradeOffer = require('steam-tradeoffer-manager/lib/classes/TradeOffer');
 ['log', 'summarize'].forEach(function (v) {
     TradeOffer.prototype[v] = require('utils/offer/' + v);
 });
-
-const log = require('lib/logger');
 
 const package = require('@root/package.json');
 
@@ -51,29 +55,21 @@ require('death')({ uncaughtException: true })(function (signal, err) {
         log.error('Create an issue here: https://github.com/Nicklason/bot-framework/issues/new');
     }
 
-    if (signal === 'SIGKILL') {
-        log.warn('Received kill signal `' + signal + '`, stopping...');
-        handler.shutdown(null, true);
-    }
-
-    // Check if it is an error (error object) or a signal (string)
-    const first = handler.shutdown(crashed ? err : null) !== false;
-
-    if (first && !crashed) {
+    if (!crashed) {
         log.warn('Received kill signal `' + signal + '`, stopping...');
     }
+
+    handler.shutdown(crashed ? err : null, signal === 'SIGKILL');
 });
 
 process.on('message', function (message) {
     if (message === 'shutdown') {
         // For using PM2 on Windows
-        const first = handler.shutdown(null) !== false;
+        log.warn('Process received shutdown message, stopping...');
 
-        if (first) {
-            log.warn('Received shutdown message, stopping...');
-        }
+        handler.shutdown(null);
     } else {
-        log.warn('Received message `' + message + '`');
+        log.warn('Process received unknown message `' + message + '`');
     }
 });
 
@@ -83,6 +79,8 @@ const async = require('async');
 // Set up node-tf2
 require('lib/tf2');
 
+const pm2 = require('pm2');
+
 const client = require('lib/client');
 const manager = require('lib/manager');
 
@@ -91,115 +89,121 @@ const listingManager = require('lib/bptf-listings');
 
 log.info(package.name + ' v' + package.version + ' is starting...');
 
-handler.onRun(function (opts) {
-    opts = opts || {};
+pm2.connect(function (err) {
+    if (err) {
+        throw err;
+    }
 
-    const loginKey = opts.loginKey || null;
+    handler.onRun(function (opts) {
+        opts = opts || {};
 
-    let lastLoginFailed = false;
+        const loginKey = opts.loginKey || null;
 
-    const login = require('app/login');
+        let lastLoginFailed = false;
 
-    log.info('Signing in to Steam...');
+        const login = require('app/login');
 
-    // Perform login
-    login(loginKey, loginResponse);
+        log.info('Signing in to Steam...');
 
-    function loginResponse (err) {
-        if (err) {
-            if (!lastLoginFailed && err.eresult !== SteamUser.EFriendRelationship.RateLimitExceeded && err.eresult !== SteamUser.EFriendRelationship.InvalidPassword) {
-                lastLoginFailed = true;
-                // Try and sign in without login key
-                log.warn('Failed to sign in to Steam, retrying without login key...');
-                login(null, loginResponse);
-            } else {
-                log.warn('Failed to sign in to Steam');
-                handler.onLoginFailure(err);
-            }
-            return;
-        }
+        // Perform login
+        login(loginKey, loginResponse);
 
-        checkAccountLimitations(function (err) {
+        function loginResponse (err) {
             if (err) {
-                throw err;
+                if (!lastLoginFailed && err.eresult !== SteamUser.EFriendRelationship.RateLimitExceeded && err.eresult !== SteamUser.EFriendRelationship.InvalidPassword) {
+                    lastLoginFailed = true;
+                    // Try and sign in without login key
+                    log.warn('Failed to sign in to Steam, retrying without login key...');
+                    login(null, loginResponse);
+                } else {
+                    log.warn('Failed to sign in to Steam');
+                    handler.onLoginFailure(err);
+                }
+                return;
             }
 
-            log.debug('Waiting for web session...');
-
-            // Wait for steamcommunity session
-            require('utils/communityLoginCallback')(false, function (err, cookies) {
+            checkAccountLimitations(function (err) {
                 if (err) {
                     throw err;
                 }
 
-                require('lib/bptf-login').setCookies(cookies);
+                log.debug('Waiting for web session...');
 
-                require('app/bptf').setup(function (err) {
+                // Wait for steamcommunity session
+                require('utils/communityLoginCallback')(false, function (err, cookies) {
                     if (err) {
                         throw err;
                     }
-                    log.info('Initializing tf2-schema...');
 
-                    schemaManager.init(function (err) {
+                    require('lib/bptf-login').setCookies(cookies);
+
+                    require('app/bptf').setup(function (err) {
                         if (err) {
                             throw err;
                         }
+                        log.info('Initializing tf2-schema...');
 
-                        log.info('tf2-schema is ready!');
-
-                        // Set access token
-                        listingManager.token = process.env.BPTF_ACCESS_TOKEN;
-                        // Set schema for bptf-listings
-                        listingManager.schema = schemaManager.schema;
-
-                        // Set steamid
-                        listingManager.steamid = client.steamID;
-                        manager.steamID = client.steamID;
-
-                        async.parallel({
-                            inventory: function (callback) {
-                                // Load inventory
-                                require('app/inventory').getInventory(client.steamID, callback);
-                            },
-                            listings: function (callback) {
-                                // Initialize bptf-listings
-                                listingManager.init(callback);
-                            }
-                        }, function (err, result) {
+                        schemaManager.init(function (err) {
                             if (err) {
                                 throw err;
                             }
 
-                            log.info('Setting up pricelist...');
+                            log.info('tf2-schema is ready!');
 
-                            require('app/prices').init(function (err) {
+                            // Set access token
+                            listingManager.token = process.env.BPTF_ACCESS_TOKEN;
+                            // Set schema for bptf-listings
+                            listingManager.schema = schemaManager.schema;
+
+                            // Set steamid
+                            listingManager.steamid = client.steamID;
+                            manager.steamID = client.steamID;
+
+                            async.parallel({
+                                inventory: function (callback) {
+                                    // Load inventory
+                                    require('app/inventory').getInventory(client.steamID, callback);
+                                },
+                                listings: function (callback) {
+                                    // Initialize bptf-listings
+                                    listingManager.init(callback);
+                                }
+                            }, function (err, result) {
                                 if (err) {
                                     throw err;
                                 }
 
-                                log.info('Creating listings...');
+                                log.info('Setting up pricelist...');
 
-                                require('handler/listings').checkAll(function (err) {
+                                require('app/prices').init(function (err) {
                                     if (err) {
                                         throw err;
                                     }
 
-                                    log.info('Getting Steam API key...');
+                                    log.info('Creating listings...');
 
-                                    // Set cookies for the tradeoffer manager which will start the polling
-                                    manager.setCookies(cookies, function (err) {
+                                    require('handler/listings').checkAll(function (err) {
                                         if (err) {
                                             throw err;
                                         }
 
-                                        require('handler/friends').getMaxFriends(function (err) {
+                                        log.info('Getting Steam API key...');
+
+                                        // Set cookies for the tradeoffer manager which will start the polling
+                                        manager.setCookies(cookies, function (err) {
                                             if (err) {
                                                 throw err;
                                             }
 
-                                            handlerManager.setReady();
+                                            require('handler/friends').getMaxFriends(function (err) {
+                                                if (err) {
+                                                    throw err;
+                                                }
 
-                                            handler.onReady();
+                                                handlerManager.setReady();
+
+                                                handler.onReady();
+                                            });
                                         });
                                     });
                                 });
@@ -208,8 +212,8 @@ handler.onRun(function (opts) {
                     });
                 });
             });
-        });
-    }
+        }
+    });
 });
 
 function checkAccountLimitations (callback) {
