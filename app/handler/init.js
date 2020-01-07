3634ebb406be77d918e0922c264ef46c68a761c0
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-01-07 14:41:26 +0100

    Fixed problem reading files on startup

--- app/handler/init.js
+++ app/handler/init.js
@@ -9,16 +9,16 @@ const handlerManager = require('app/handler-manager');
 module.exports = function (done) {
     async.parallel({
         loginKey: function (callback) {
-            files.readFile(paths.loginKey, callback);
+            files.readFile(paths.files.loginKey, callback);
         },
         pollData: function (callback) {
-            files.readFile(paths.pollData, true, callback);
+            files.readFile(paths.files.pollData, true, callback);
         },
         loginAttempts: function (callback) {
-            files.readFile(paths.loginAttempts, true, callback);
+            files.readFile(paths.files.loginAttempts, true, callback);
         },
         pricelist: function (callback) {
-            files.readFile(paths.pricelist, true, callback);
+            files.readFile(paths.files.pricelist, true, callback);
         }
     }, function (err, data) {
         if (err) {
