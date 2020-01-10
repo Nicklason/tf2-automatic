049c1c770d2e6eee8439feb3f4663faf40df45d6
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2020-01-10 21:34:32 +0100

    Added variable for 'DISCORD_WEBHOOK_URL'

--- app/discord.js
+++ app/discord.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
-const hookid = process.env.DISCORD_WEBHOOK_URl.split('/')[5];
-const hooktoken = process.env.DISCORD_WEBHOOK_URl.split('/')[6];
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL || "";
+const hookid = DISCORD_WEBHOOK_URL.split('/')[5];
+const hooktoken = DISCORD_WEBHOOK_URL.split('/')[6];
 const hook = new Discord.WebhookClient(hookid, hooktoken);
 
 const log = require('lib/logger');
