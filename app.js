df46d3bd0be8d327e454d20299cda05c85f05718
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-01-11 20:00:37 +0100

    :bug: Fix link to create an issue on GitHub

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ require('death')({ uncaughtException: true })(function (signal, err) {
             require('util').inspect(err)
         ].join('\r\n'));
 
-        log.error('Create an issue here: https://github.com/Nicklason/bot-framework/issues/new');
+        log.error('Create an issue here: https://github.com/Nicklason/tf2-automatic/issues/new?template=bug_report.md');
     }
 
     if (!crashed) {
