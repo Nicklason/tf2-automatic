522ccdd41c5a74dda2aa8642dd8e366dc9199b7f
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-01-23 20:23:20 +0100

    Add types for tf2-sku

---
+++ src/types/modules/tf2-sku/index.d.ts
@@ -0,0 +1,39 @@
+declare module 'tf2-sku' {
+    export function fromObject (item: {
+        defindex: number,
+        quality: number,
+        craftable?:  boolean,
+        killstreak?: number,
+        australium?: boolean,
+        effect?: number,
+        festive?: boolean,
+        paintkit?: number,
+        wear?: number,
+        quality2?: number,
+        craftnumber?: number,
+        crateseries?: number,
+        paint?: number,
+        target?: number,
+        output?: number,
+        outputQuality?: number
+    }): string;
+
+    export function fromString (sku: string): {
+        defindex: number,
+        quality: number,
+        craftable: boolean,
+        killstreak: number,
+        australium: boolean,
+        effect: number,
+        festive: boolean,
+        paintkit: number,
+        wear: number,
+        quality2: number,
+        craftnumber: number,
+        crateseries: number,
+        paint: number,
+        target: number,
+        output: number,
+        outputQuality: number
+    }
+}
