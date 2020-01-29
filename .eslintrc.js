799f1b2e6c4bccd2d836cdd1eda3030cd01cf223
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-01-30 00:04:22 +0100

    lint: add tsdoc plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc'],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
@@ -13,6 +13,7 @@ module.exports = {
         'lines-between-class-members': ['error', 'always'],
         '@typescript-eslint/no-explicit-any': [0],
         '@typescript-eslint/ban-ts-ignore': [0],
-        '@typescript-eslint/no-use-before-define': [0]
+        '@typescript-eslint/no-use-before-define': [0],
+        'tsdoc/syntax': 'warn'
     }
 };
