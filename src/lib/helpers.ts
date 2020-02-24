8c765e65201eed5a9653e28ebbad3861fe6b21c9
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-02-24 16:23:41 +0100

    Upgrade parseJSON with UnknownDictionaryKnownValue

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,8 +1,10 @@
+import { UnknownDictionaryKnownValues } from '../types/common';
+
 export function exponentialBackoff(n: number, base = 1000): number {
     return Math.pow(2, n) * base + Math.floor(Math.random() * base);
 }
 
-export function parseJSON(json: string): object | any[] | null {
+export function parseJSON(json: string): UnknownDictionaryKnownValues | null {
     try {
         return JSON.parse(json);
     } catch (err) {
